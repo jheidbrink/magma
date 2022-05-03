f0fefa772e3567a1e41b3a62ba2fdf2ecd9a84ed
Jan Heidbrink <jan.heidbrink@tngtech.com>
2022-05-03 18:40:41 +0200

    Nix: Bazel can build Python services in a nix shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,19 @@
+# I'm currently invoking shell.nix like this: pkill --full 'bazel.*workspace_directory=/home/heidbrij/repositories/github.com/jheidbrink/magma'; while pgrep --full 'bazel.*workspace_directory=/home/heidbrij/repositories/github.com/jheidbrink/magma'; do sleep 0.03; done; echo 'bazel build --sandbox_debug --define=folly_so=1 //orc8r/gateway/python/magma/magmad:magmad' | nix-shell --pure
 { pkgs ? import <nixpkgs> {} }:
+let
+  bazel = pkgs.writers.writeDashBin "bazel" ''
+    export GIT_SSL_CAINFO="/etc/ssl/certs/ca-bundle.crt"
+    ${pkgs.bazel_4}/bin/bazel $@
+  '';  # ca-bundle.crt is provided by cacert package and put into place by buildFHSUserEnv. It's needed by git in order to clone/fetch.
+in
 (pkgs.buildFHSUserEnv {
   name = "magma";
   targetPkgs = pkgs: [
-    pkgs.bazel_4
+    bazel
+    pkgs.systemd.dev  # found via `nix-locate sd-daemon.h`, required for build Python wheel systemd
+    pkgs.gcc
+    pkgs.python38
+    pkgs.git
+    pkgs.cacert
   ];
 }).env
